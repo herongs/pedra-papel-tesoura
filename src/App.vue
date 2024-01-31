8c11caad73fb9285071229fdccac8afc2c90ae75
Heron Slovinski <heronslovinski@hotmail.com>
2024-01-30 21:08:31 -0300

    modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js     modified:   public/index.html 	modified:   src/App.vue 	new file:   src/assets/design/bonus/desktop-rules-modal-bonus.jpg 	new file:   src/assets/design/bonus/desktop-step-1-bonus.jpg 	new file:   src/assets/design/bonus/desktop-step-2-bonus.jpg 	new file:   src/assets/design/bonus/desktop-step-3-bonus.jpg 	new file:   src/assets/design/bonus/desktop-step-4-bonus.jpg 	new file:   src/assets/design/bonus/mobile-rules-modal-bonus.jpg 	new file:   src/assets/design/bonus/mobile-step-1-bonus.jpg 	new file:   src/assets/design/bonus/mobile-step-2-bonus.jpg 	new file:   src/assets/design/bonus/mobile-step-3-bonus.jpg 	new file:   src/assets/design/bonus/mobile-step-4-bonus.jpg 	new file:   src/assets/design/desktop-preview.jpg 	new file:   src/assets/design/original/desktop-rules-modal.jpg 	new file:   src/assets/design/original/desktop-step-1.jpg 	new file:   src/assets/design/original/desktop-step-2.jpg 	new file:   src/assets/design/original/desktop-step-3.jpg 	new file:   src/assets/design/original/desktop-step-4-lose.jpg 	new file:   src/assets/design/original/desktop-step-4-win.jpg 	new file:   src/assets/design/original/mobile-rules-modal.jpg 	new file:   src/assets/design/original/mobile-step-1.jpg 	new file:   src/assets/design/original/mobile-step-2.jpg 	new file:   src/assets/design/original/mobile-step-3.jpg 	new file:   src/assets/design/original/mobile-step-4-lose.jpg 	new file:   src/assets/design/original/mobile-step-4-win.jpg 	new file:   src/assets/images/bg-pentagon.svg 	new file:   src/assets/images/bg-triangle.svg 	new file:   src/assets/images/favicon-32x32.png 	new file:   src/assets/images/icon-close.svg 	new file:   src/assets/images/icon-lizard.svg 	new file:   src/assets/images/icon-paper.svg 	new file:   src/assets/images/icon-rock.svg 	new file:   src/assets/images/icon-scissors.svg 	new file:   src/assets/images/icon-spock.svg 	new file:   src/assets/images/image-rules-bonus.svg 	new file:   src/assets/images/image-rules.svg 	new file:   src/assets/images/logo-bonus.svg 	new file:   src/assets/images/logo.svg 	deleted:    src/assets/logo.png 	new file:   src/index.css 	modified:   src/main.js 	new file:   tailwind.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,160 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex justify-center pt-6">
+    <div class="cabecalho">
+      <div>
+        <img src="./assets/images/logo.svg" alt="" />
+      </div>
+      <div class="box-score">
+        <span class="titulo text-blue-600">SCORE</span> <br />
+        <span class="titulo1 text-gray-600">12</span>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify-center pt-6">
+    <div class="p-6" @click="selectPlay(1)">Pedra</div>
+    <div class="p-6" @click="selectPlay(2)">Papel</div>
+    <div class="p-6" @click="selectPlay(3)">Tesoura</div>
+  </div>
+
+  <div class="flex justify-center pt-40">
+    <img src="./assets/images/bg-triangle.svg" class="triangle" alt="">
+
+
+    <img src="./assets/images/icon-rock.svg" class="pedra x" alt="">
+    <img src="./assets/images/icon-paper.svg" class="papel x" alt="">
+    <img src="./assets/images/icon-scissors.svg" class="tesoura x" alt="">
+  </div>
+
+  <div class="flex justify-center pt-6">
+    <div class="p-6" v-if="playerChoice">Você escolheu {{ playerChoice }}</div>
+    <div class="p-6" v-if="computerChoice">O computador escolheu {{ computerChoice }}</div>
+    <div class="p-6" v-if="resultado">Resultado: {{ resultado }}</div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const playerChoice = ref(null);
+const computerChoice = ref(null);
+const resultado = ref(null);
+
+const results = {
+  "1-1": "Empate",
+  "1-2": "Perdeu",
+  "1-3": "Ganhou",
+  "2-1": "Ganhou",
+  "2-2": "Empate",
+  "2-3": "Perdeu",
+  "3-1": "Perdeu",
+  "3-2": "Ganhou",
+  "3-3": "Empate",
+};
+
+const startGame = () => {
+  if (playerChoice.value && computerChoice.value) {
+    const resultKey = `${playerChoice.value}-${computerChoice.value}`;
+    resultado.value = results[resultKey];
+
+    console.log(resultado.value);
   }
-}
+};
+
+const selectPlay = (play) => {
+  playerChoice.value = play;
+
+  if (playerChoice.value) {
+    computerChoice.value = Math.floor(Math.random() * 3) + 1;
+  }
+
+  startGame();
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.cabecalho {
+  background-color: transparent;
+  padding: 30px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  border: 2px solid #dddcdca1;
+  display: flex;
+  justify-content: space-between;
+  width: 1000px;
+}
+
+.box-score {
+  background-color: #f2f2f2;
+  width: 120px;
+  height: 100px;
+  border-radius: 10px;
+}
+
+.titulo {
+  font-size: 12px;
+  font-weight: bold;
+}
+
+.titulo1 {
+  font-size: 55px;
+  font-weight: bold;
+}
+
+.triangle{
+
+  z-index: 1;
+  width: 400px;
+}
+
+.pedra {
+  z-index: 2;
+  position: absolute;
+  transform: translate(0%, 170%);
+  width: 150px;
+  background-color: #f2f2f2;
+  border-radius: 50%;
+  border: 20px solid #D94169;
+  padding: 35px; 
+  box-shadow: inset 0px 4px 5px  rgba(0, 0, 0, 0.5), 0px 6px 0px #e21048;
+  cursor: pointer;
+
+}
+
+
+.tesoura {
+  z-index: 2;
+  position: absolute;
+  transform: translate(100%, -40%);
+  width: 150px;
+  background-color: #f2f2f2;
+  border-radius: 50%;
+  border: 20px solid #F2AB27;
+  padding: 35px;
+  box-shadow: inset 0px 4px 5px  rgba(0, 0, 0, 0.5), 0px 6px 0px #ec9c07;
+  cursor: pointer;
+}
+
+.papel {
+  z-index: 2;
+  position: absolute;
+  transform: translate(-110%, -40%);
+  width: 150px;
+  background-color: #f2f2f2;
+  border-radius: 50%;
+  border: 20px solid #577EF2;
+  padding: 35px;
+  box-shadow: inset 0px 4px 5px rgba(0, 0, 0, 0.5), 0px 6px 0px #1749df;
+  cursor: pointer;
+}
+
+.x {
+transition: all 0.5s;
+}
+
+.x:hover {
+  transform: scale(1.1);
 }
 </style>
